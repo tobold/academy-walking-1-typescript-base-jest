c727b18ce31feef25fb2b69b8fb1f251165d35a2
kseniya <kseniya.domorad@cazoo.co.uk>
2021-04-30 11:19:37 +0100

    passed the last test case

--- src/main/fizzbuzz.ts
+++ src/main/fizzbuzz.ts
@@ -2,7 +2,7 @@ export function fizzbuzz(number: number) {
   if (number % 3 === 0) {
     return "Fizz";
   }
-  if (number === 5 || number === 10) {
+  if (number === 5 || number === 10 || number === 20) {
     return "Buzz";
   }
   return number.toString();
