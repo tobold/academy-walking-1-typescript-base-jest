2c9628b2205a21c578f212b926a655b8e8b7d4e7
James Cooke <james.cooke@cazoo.co.uk>
2021-07-16 09:50:33 +0100

    adding next test

--- src/Rover.test.ts
+++ src/Rover.test.ts
@@ -12,4 +12,11 @@ describe('Rover', () => {
     const newPosition: Position = rover.manoeuver('M');
     expect(newPosition).toEqual({x: 0, y: 1, direction: Direction.North})
   });
+  it('should accept two move commands and return its position', () => {
+    const grid: Grid = {length: 10, width: 10}
+    const startingPosition: Position = {direction: Direction.North, x: 0, y: 0};
+    const rover = new Rover(grid, startingPosition)
+    const newPosition: Position = rover.manoeuver('MM');
+    expect(newPosition).toEqual({x: 0, y: 2, direction: Direction.North})
+  });
 });
