74875cf0540eb025d2cba6d92023a5bf1426f586
Toby Hamand <toby.hamand@cazoo.co.uk>
2021-07-16 11:02:09 +0200

    handle moving twice

--- src/Rover.test.ts
+++ src/Rover.test.ts
@@ -1,22 +1,17 @@
-import {Position, Grid, Direction, Rover} from "./Rover";
-
-
-
-
+import { Direction, Grid, Position, Rover } from "./Rover";
 
 describe('Rover', () => {
-  it('should accept a move command and return its position', () => {
+  it.each([{
+    input: 'M',
+    output: {x: 0, y: 1, direction: Direction.North}
+  }, {
+    input: 'MM',
+    output: {x: 0, y: 2, direction: Direction.North}
+  }])('should accept a move command and return its position', ({input, output}) => {
     const grid: Grid = {length: 10, width: 10}
     const startingPosition: Position = {direction: Direction.North, x: 0, y: 0};
     const rover = new Rover(grid, startingPosition)
-    const newPosition: Position = rover.manoeuver('M');
-    expect(newPosition).toEqual({x: 0, y: 1, direction: Direction.North})
-  });
-  it('should accept two move commands and return its position', () => {
-    const grid: Grid = {length: 10, width: 10}
-    const startingPosition: Position = {direction: Direction.North, x: 0, y: 0};
-    const rover = new Rover(grid, startingPosition)
-    const newPosition: Position = rover.manoeuver('MM');
-    expect(newPosition).toEqual({x: 0, y: 2, direction: Direction.North})
+    const newPosition: Position = rover.manoeuver(input);
+    expect(newPosition).toEqual(output)
   });
 });
--- src/Rover.ts
+++ src/Rover.ts
@@ -1,31 +1,39 @@
 export interface Grid {
-    width: number
-    length: number
+  width: number
+  length: number
 }
 
 export enum Direction {
-    North,
-    East,
-    South,
-    West,
+  North,
+  East,
+  South,
+  West,
 }
 
 export interface Position {
-    x: number
-    y: number
-    direction: Direction
+  x: number
+  y: number
+  direction: Direction
+}
+
+enum Command {
+  M = 'M'
 }
 
 export class Rover {
-    private grid: Grid;
-    private startingPosition: Position;
-    constructor(grid: Grid, startingPosition: Position) {
-        this.grid = grid;
-        this.startingPosition = startingPosition;
-    }
+  private grid: Grid;
+  private position: Position
 
-
-    manoeuver(movement: string): Position {
-        return {direction: Direction.North, x: 0, y: 1};
-    }
+  constructor(grid: Grid, startingPosition: Position) {
+    this.grid = grid;
+    this.position = startingPosition;
+  }
+  
+  manoeuver(movement: string): Position {
+    const commands = movement.split('') as Command[]
+    commands.forEach(command => {
+      this.position.y += 1
+    })
+    return this.position;
+  }
 }
