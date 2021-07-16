6085af56fa69848eda64b2eee97fcc74fed01964
Toby Hamand <toby.hamand@cazoo.co.uk>
2021-07-16 11:26:06 +0200

    turning test

--- src/Rover.test.ts
+++ src/Rover.test.ts
@@ -3,8 +3,9 @@ import { Direction, Grid, Position, Rover } from "./Rover";
 describe('Rover', () => {
   it.each([
     ['M', {x: 0, y: 1, direction: Direction.North}],
-    ['MM', {x: 0, y: 2, direction: Direction.North}
-  ]])
+    ['MM', {x: 0, y: 2, direction: Direction.North}],
+    ['R', {x: 0, y: 0, direction: Direction.East}]
+  ])
   ('should accept %s and return %s', (input, output) => {
     const grid: Grid = {length: 10, width: 10}
     const startingPosition: Position = {direction: Direction.North, x: 0, y: 0};
